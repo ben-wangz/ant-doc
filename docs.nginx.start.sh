054c808accb6e0161b5ce0670c8896f3ceebbc9e
ben.wangz <ben.wangz@foxmail.com>
2021-08-10 19:41:00 +0800

    add port parameter

--- docs.nginx.start.sh
+++ docs.nginx.start.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 CONTAINER_NAME=${1:-docs}
+PORT=${2:-8080}
 
 SCRIPT_DIRECOTRY=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-docker run --rm --name ${CONTAINER_NAME} -p 8080:80 -v ${SCRIPT_DIRECOTRY}/docs/:/usr/share/nginx/html:ro -v ${SCRIPT_DIRECOTRY}/docs/default.conf:/etc/nginx/conf.d/default.conf:ro -d nginx:1.19.9-alpine
+docker run --rm --name ${CONTAINER_NAME} -p ${PORT}:80 -v ${SCRIPT_DIRECOTRY}/docs/:/usr/share/nginx/html:ro -v ${SCRIPT_DIRECOTRY}/docs/default.conf:/etc/nginx/conf.d/default.conf:ro -d nginx:1.19.9-alpine
